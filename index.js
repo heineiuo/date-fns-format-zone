11ffa622e1e69d18ed68b0dc309b0a990dc47663
heineiuo <heineiuo@gmail.com>
2018-03-27 12:31:40 +0800

    elimination of ambiguity

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const zoneMap = require('./timezone-parse')
 
 const format = function (_format) {
-  return function (_date, format, options) {
+  return function (_date, fmt, options) {
     var date = _date
     if (options.zone) {
       date = new Date(_date)
       date = new Date(date.getTime() + zoneMap[options.zone])
       delete options.zone
     }
-    return _format(date, format, options)
+    return _format(date, fmt, options)
   }
 }
 
